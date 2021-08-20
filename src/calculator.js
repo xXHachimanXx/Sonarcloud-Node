6a007e57a0be1a9119a5cda92a27f7233b8db46d
xXHachimanXx <globaldep90@gmail.com>
2021-08-20 19:29:24 -0300

    feat: Adding app and tests.

---
+++ src/calculator.js
@@ -0,0 +1,22 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+     return a - b
+}
+
+function times(a, b) {
+     return a * b
+}
+
+function sumX(a, b) {
+     return a + b + a
+}
+
+module.exports = {
+    sum,
+    sub,
+    times,
+    sumX
+};
